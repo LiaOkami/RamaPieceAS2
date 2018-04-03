b24dfda54cabe58d49f75932154233e59bb8affc
Nicolas Charlon <charlon.nicolas@gmail.com>
2018-04-03 16:20:57 +0200

    add function to count coins

---
+++ include/optimisation/compte.h
@@ -0,0 +1,8 @@
+#ifndef FONCTION_H_INCLUDED
+#define FONCTION_H_INCLUDED
+
+void RamasCoinMax(int);
+
+void RamasAlternative(int);
+
+#endif // FONCTION_H_INCLUDED
--- src/optimisation/main.cpp
+++ src/optimisation/main.cpp
@@ -1,4 +1,14 @@
-int	main()
+#include <iostream>
+#include "compte.h"
+using namespace std;
+
+int main()
 {
-  return (0);
+    int som;
+    cout << "Quelle somme voulez vous en centimes ?" << endl;
+    cin>>som;
+    RamasCoinMax(som);
+    cout<<endl;
+
+    return 0;
 }
