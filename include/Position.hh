b662bfcfdfc0a4ac0e50ba5260d1a56b96ccd40b
Okami <pierrick.garcia@hotmail.fr>
2018-04-03 14:19:06 +0200

    Add Position Struct

---
+++ include/Position.hh
@@ -0,0 +1,30 @@
+/** \file Position.hh
+ *  \brief Classe Position
+ */
+
+#ifndef _POSITION_HH_
+# define _POSITION_HH_
+
+# include <stdint.h>
+
+/** \brief Structure représentant la Position d'un Object
+ *  \author Pierrick Garcia
+ */
+struct Position
+{
+  double	x;
+  double	y;
+
+  Position(double xx = 0, double yy = 0) { x=xx; y=yy;}
+};
+
+/** \brief Structure représentant une Position en terme d'entiers positifs
+ *  \author Pierrick Garcia
+ */
+struct PositionInt
+{
+  unsigned int	x;
+  unsigned int	y;
+};
+
+#endif
