8d3b2b67ed42721a15598e5bb119eed235352963
Okami <pierrick.garcia@hotmail.fr>
2018-04-09 16:09:03 +0200

    Add Piece struct

---
+++ include/Piece.hh
@@ -0,0 +1,22 @@
+/** \file Piece.hh
+ *  \brief Classe Piece
+ */
+
+#ifndef _PIECE_HH_
+# define _PIECE_HH_
+
+/** \brief Structure représentant une Piece
+ *  \author Pierrick Garcia
+ */
+struct Piece
+{
+    int         value;
+    Position    pos;
+
+    Piece(int val = 0, Position po = Position())
+      : value(val), pos(po) {}
+    Piece(int val, double x, double y)
+      : value(val), pos(Position(x, y)) {}
+};
+
+#endif
