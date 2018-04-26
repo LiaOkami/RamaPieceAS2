1b832d0984b76d34aeb154f90009a0600b7d8c29
Stephane <stephane.achari@u-psud.fr>
2018-04-26 22:38:43 +0200

    MAJ parcours.cpp

--- include/optimisation/parcours.h
+++ include/optimisation/parcours.h
@@ -3,8 +3,12 @@
 
 vector<piece> triDistancePieceBras(Ramapiece p);
 vector<piece> triDistancePiecePiece(const std::vector<int> & tabP);
+vector<Piece> triValeur(const std::vector<int> & tabP);
+int rechercheDicho(const std::vector<int> & tabP, int val);
+
 void parcoursSimple(Ramapiece p);
 void parcoursDesVoisins(Ramapiece p);
-
+void parcoursCirculaire(Ramapiece p);
+void parcoursZone(Ramapiece p);
 
 #endif // PARCOURS_H_INCLUDED
--- src/optimisation/parcours.cpp
+++ src/optimisation/parcours.cpp
@@ -19,7 +19,7 @@
  *         tableau utile si on part du principe que le bras reviens a sa position apres chaque piece
  *  \return vector
  */
-vector<piece> triDistancePieceBras(Ramapiece p){
+vector<Piece> triDistancePieceBras(Ramapiece p){
         Piece tmpPiece;
         int j = 0, i = 0; indicePetit = 0;
         double valPetit = 0.0;
@@ -53,9 +53,9 @@ vector<piece> triDistancePieceBras(Ramapiece p){
  * \param tableau de piece (exemple p.getPieces(), ou p est de type Ramapiece)
  * \return vector
  */
-vector<piece> triDistancePiecePiece(const std::vector<int> & tabP){
+vector<Piece> triDistancePiecePiece(const std::vector<int> & tabP){
 
-        vector<Piece> tabPiece = tabP; /*on sait que la premiere piece sera la plus proche du bras et on fait par rapport a elle maintenant*/
+        vector<Piece> tabPiece = tabP;
 
         Piece tmpPiece;
         int j = 0, i = 0; indicePetit = 0;
@@ -94,6 +94,107 @@ vector<piece> triDistancePiecePiece(const std::vector<int> & tabP){
         return tabPiece;
 }
 
+/**
+ *  \brief Algorithme de tri, retourne un vector de piece ordonnée par leur valeur croissante,
+ *          peut servir à une recherche dichotomique
+ * \param tableau de piece (exemple p.getPieces(), ou p est de type Ramapiece)
+ * \return vector
+ */
+vector<Piece> triValeur(const std::vector<int> & tabP){
+
+        vector<Piece> tabPiece = tabP;
+
+        Piece tmpPiece;
+        int j = 0, i = 0; indicePetit = 0;
+        int valPetit = 0;
+
+        for(i = 0; i<p.tabPiece.size(); i++){
+
+            valPetit =  tabPiece[j]->value;
+            indicePetit = i;
+
+            for(j = i; j<tabInt.size(); j++){
+                if(tabPiece[j]->value < valPetite){
+                    indicePetit = j;
+                    valPetit = tabPiece[j]->value);
+                }
+            }
+            /*on echange*/
+            tmpPiece = tabPiece[i];
+            tabPiece[i] = valPetit;
+            tabPiece[indicePetit] = tmp;
+        }
+
+        return tabPiece;
+}
+
+/**
+ *  \brief Algorithme de tri, retourne un vector de piece ordonnée par leur valeur croissante,
+ *          peut servir à une recherche dichotomique
+ * \param tableau de piece (exemple p.getPieces(), ou p est de type Ramapiece)
+ * \return vector
+ */
+vector<Piece> triValeur(const std::vector<int> & tabP){
+
+        vector<Piece> tabPiece = tabP;
+
+        Piece tmpPiece;
+        int j = 0, i = 0; indicePetit = 0;
+        int valPetit = 0;
+
+        for(i = 0; i<p.tabPiece.size(); i++){
+
+            valPetit =  tabPiece[j]->value;
+            indicePetit = i;
+
+            for(j = i; j<tabInt.size(); j++){
+                if(tabPiece[j]->value < valPetite){
+                    indicePetit = j;
+                    valPetit = tabPiece[j]->value);
+                }
+            }
+            /*on echange*/
+            tmpPiece = tabPiece[i];
+            tabPiece[i] = valPetit;
+            tabPiece[indicePetit] = tmp;
+        }
+
+        return tabPiece;
+}
+
+/**
+ *  \brief Tri dichotomique pour retrouver une piece d'une certaine valeur,
+ * \param tableau de piece (exemple p.getPieces(), ou p est de type Ramapiece)
+ * \param valeur de la piece recherche
+ * \return int -> indice ou trouver la piece, si non trouve retourne -999
+ */
+int rechercheDicho(const std::vector<int> & tabP, int val){
+
+        vector<Piece> tabPiece = triValeur(tabP);
+
+        int indiceMax = tabPiece.size(), indiceMin = 0, indiceMil = 0;
+        bool pieceTrouve = false;
+
+        while(!pieceTrouve && (indiceMax - indiceMin) > 1){
+            indiceMil = (indiceMax + indiceMin )/2;
+            if(tabPiece[indiceMil]->value = val){
+                pieceTrouve = trouve;
+            }
+            else if(val > tabPiece[indiceMil]->value){
+                indiceMin = indiceMil;
+            }
+            else{
+                indiceMax = indiceMil - 1;
+            }
+        }
+
+        if(pieceTrouve){
+            return tabPiece[indiceMil];
+        }
+        else{
+            return -999;
+        }
+}
 
 /**
  *  \brief Parcours basique en partant du principe que le bras ramasse une piece
@@ -127,6 +228,10 @@ void parcoursDesVoisins(Ramapiece p){
  *  \brief On parcours de telle manière a faire des cercles pour se rapprocher du centre
  */
 void parcoursCirculaire(Ramapiece p){
+}
 
-
+/**
+ *  \brief Parcours par dans une zone définie puis change de zone
+ */
+void parcoursZone(Ramapiece p){
 }
