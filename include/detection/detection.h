207a2876b467fbff904f13b2aa23bd7cda64e1d3
Etienne <etienne.berrie@u-psud.fr>
2018-05-07 14:00:25 +0200

    Création Classe Detection

---
+++ include/detection/detection.h
@@ -0,0 +1,17 @@
+#ifndef DETECTION_H_INCLUDED
+#define DETECTION_H_INCLUDED
+
+#include "Piece.hh"
+
+/** \brief Classe permettant la detection des pièces sur le tapis
+ *  \author
+ */
+
+class Detection{
+private:
+    vector<Piece> listePieceActuelle;
+public:
+    Detection();
+};
+
+#endif // DETECTION_H_INCLUDED
