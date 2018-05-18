ed6c273cb1984e878248e6a6055a770acd6062c1
Etienne <etienne.berrie@u-psud.fr>
2018-05-18 16:21:52 +0200

    Remove Function tracerPiece3points from Class Detection

--- src/detection/detection.cpp
+++ src/detection/detection.cpp
@@ -50,33 +50,39 @@ Detection::getListePieceCourante()
 
 void Detection::detectionPieces()
 {
-    //Prend l'image (avec Cany edge detector (N)
+    //Prend l'image (avec Cany edge detector (Nicolas)
 
     //transforme en tableau de points blancs (N)
 
-    /* boucle pour detecter tous les cercles, pour plus tard*/
-        //on trace un cercle avec trois points tirés aléatoirement (E)
-        //nombres aléatoires
-        int MIN = 0, MAX = 10; //MAX = tableau.lenght()
-        srand(time(NULL));
-        int alea1 = rand()%(MAX-MIN) + MIN,
-            alea2 = rand()%(MAX-MIN) + MIN,
-            alea3 = rand()%(MAX-MIN) + MIN;
-        cout << "Nombres aléatoires : " << alea1 << ", " << alea2 << ", " << alea3 << endl; //test
-        //Piece piecetracée = traceCercle();
+    // On cherche les pièce du tapis (Etienne)
+    /* --- boucle pour detecter toutes les pièces du tapis, pour plus tard ---*/
+        for(int i = 0; i < 3; i++){
+            //on trace un cercle avec trois points tirés aléatoirement (E)
+            //nombres aléatoires
+            int MIN = 0, MAX = 10; //MAX = tableau.lenght()
+            srand(time(NULL));
+            int alea1 = rand()%(MAX-MIN) + MIN,
+                alea2 = rand()%(MAX-MIN) + MIN,
+                alea3 = rand()%(MAX-MIN) + MIN;
+            cout << "Nombres aléatoires : " << alea1 << ", " << alea2 << ", " << alea3 << endl; //test
+            Piece piecetracée = tracerPiece3points(pos1,pos2,pos3); //prendre les positions à partir du tableau de points
 
-        //On compare le centre du cercle avec chaque point du tableau > on enregistre le nombre de point blanc qu'il comporte (E)
-        //Boucle pour chaque point blanc
-
-        //On  Prend le cercle avec le plus de points comme le "vrai" cercle
+            //On compare le centre du cercle avec chaque point du tableau > on enregistre le nombre de point blanc qu'il comporte (E)
+            //Boucle pour chaque point blanc
+        }
+        //On Cherche le cercle avec le plus de points comme le "vrai" cercle
         //BOucle pour avec une donnée "max"
         //listePieceActuel.push_back(pieceCourante);
-    /* ------- */
+    /* --- Fin boucle ---- */
+}
+
+void Detection::afficherPieces()
+{
+    //TODO
 }
 
 Piece Detection::tracerPiece3points(Position A, Position B, Position C)
 {
-
     double yDelta_a = B.y - A.y;
     double xDelta_a = B.x - A.x;
     double yDelta_b = C.y - B.y;
@@ -93,8 +99,3 @@ Piece Detection::tracerPiece3points(Position A, Position B, Position C)
     return new Piece(0, centreX, centreY); //àMODIFIER : doit aussi retourner le rayon de la pièce
 }
 
-void Detection::afficherPieces()
-{
-    //TODO
-}
-}
