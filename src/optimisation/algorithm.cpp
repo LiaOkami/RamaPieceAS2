f2d7f33e0603f4fc65c7fc57b98626a5c778e79a
Okami <pierrick.garcia@hotmail.fr>
2018-05-23 17:16:27 +0200

    Fix indentation in algorithm.cpp

--- src/optimisation/algorithm.cpp
+++ src/optimisation/algorithm.cpp
@@ -1,14 +1,14 @@
 #include "algorithm.hh"
 
 const t_algorithm   Algorithm::algos[] = {
-        {"Parcours Simple", &parcoursSimple},
-        {"Parcours des Voisins", &parcoursDesVoisins},
-        {"Parcours des Voisins avec zone", &parcoursDesVoisinsZone},
-        {"Parcours par zones", &parcoursZone},
-        {"", nullptr}
-    };
+    {"Parcours Simple", &parcoursSimple},
+    {"Parcours des Voisins", &parcoursDesVoisins},
+    {"Parcours des Voisins avec zone", &parcoursDesVoisinsZone},
+    {"Parcours par zones", &parcoursZone},
+    {"", nullptr}
+};
 
 const t_algorithm_money Algorithm::algosMoney[] = {
-        {"", nullptr}
-    };
+    {"", nullptr}
+};
 
