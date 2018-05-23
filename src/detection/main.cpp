8b21da5b4eba5b5b3131ecae805e57885f14a73d
Etienne <etienne.berrie@u-psud.fr>
2018-05-23 16:44:42 +0200

    Fix tracerPiece3points function

--- src/detection/main.cpp
+++ src/detection/main.cpp
@@ -15,5 +15,13 @@ int main()
     cout << "Detection" << endl;
     Detection detect;
     detect.detectionPieces();
+
+    //Test tracerCercle
+    //Position pos1(5.0,7.0), pos2(-2.0,6.0), pos3(2.0, -8.0);
+    //Position pos1(5.0,7.0), pos2(5.0,6.0), pos3(2.0, -8.0);
+    /*Position pos1(5.0,7.0), pos2(4.0,6.0), pos3(2.0, 6.0);
+
+    Piece p = tracerPiece3points(pos1, pos2, pos3);
+    cout << "Résultat : " << p.pos.x << " " << p.pos.y << endl;*/
     return (0);
 }
