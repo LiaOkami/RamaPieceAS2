f4a5b2e8e271f1a0db774ff91f8acf266298de81
Okami <pierrick.garcia@hotmail.fr>
2018-05-25 21:18:45 +0200

    Add unit Tests for algorithms

--- src/optimisation/unitTest.cpp
+++ src/optimisation/unitTest.cpp
@@ -1,4 +1,32 @@
+#include "Algorithm.hh"
+#include "generateRamapiece.hh"
+#include "unitTest.hh"
+
 void    unitTest()
 {
+    std::vector<int>            average;
+    std::vector<Ramapiece *>    test;
+    int i;
+    int tries;
 
+    i = 0;
+    while (Algorithm::algos[i].first != "")
+    {
+        average.push_back(0);
+        i += 1;
+    }
+    for (tries = 0 ; tries < NB_TRIES ; tries++) {
+        test.push_back(generateRamapiece(50));
+        for (i = 1 ; i < average.size() ; i++) {
+            test.push_back(new Ramapiece(*(test.front())));
+        }
+        for (i = 0 ; i < average.size() ; i++) {
+            Algorithm::algos[i].second(*test[i]);
+            average[i] = (average[i] * tries + test[i]->getTraveledDistance()) / (tries + 1);
+        }
+        test.clear();
+    }
+    std::cout << "Moyenne sur " << NB_TRIES << " essais" << std::endl;
+    for (i = 0 ; i < average.size() ; i++)
+        std::cout << Algorithm::algos[i].first << " : " << average[i] << std::endl;
 }
