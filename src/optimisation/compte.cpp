b629699aea0cf57cdc5b11f44577babc0d19f95b
Nicolas Charlon <charlon.nicolas@gmail.com>
2018-06-06 15:03:07 +0200

    Made some identations

--- src/optimisation/compte.cpp
+++ src/optimisation/compte.cpp
@@ -10,18 +10,20 @@ using namespace std;
  bool isEmpty(vector<int> kindPieces){
     bool Vide = true;
 
-for(int k=0;k<=8;k++){
+    for(int k=0;k<=8;k++){
 
-    if(kindPieces[k]>0){
+        if(kindPieces[k]>0){
 
-         Vide = false;
+            Vide = false;
 }
 }
-return Vide;
+    return Vide;
 }
 
-// C'est mal indenté tu vas te faire manger si tu corriges pas ça avant vendredi
+
+
 void RamaDoable(Ramapiece &p, int som, vector<int> &kindPiecesOriginal) {
+
     vector<Piece> *pieces = p.getPieces();
     vector<int>   kindPieces(8);
     int sumTabPieces = 0;
@@ -32,54 +34,50 @@ void RamaDoable(Ramapiece &p, int som, vector<int> &kindPiecesOriginal) {
         kindPieces[i]=0;
         kindPiecesOriginal.push_back(0);
     }
-
     //On remplit les tableaux selon les valeurs des pieces sur le tapis
+
     for(int i = 0;i < pieces->size(); i++){
-
-
             switch((*pieces)[i].value){
-    case 1 :
+        case 1 :
             kindPieces[0]+=1;
             kindPiecesOriginal[0]+=1;
             break;
-    case 2 :
+        case 2 :
             kindPieces[1]+=1;
             kindPiecesOriginal[1]+=1;
             break;
-    case 5 :
+        case 5 :
             kindPieces[2]+=1;
             kindPiecesOriginal[2]+=1;
             break;
-    case 10 :
+        case 10 :
             kindPieces[3]+=1;
             kindPiecesOriginal[3]+=1;
             break;
-    case 20 :
+        case 20 :
             kindPieces[4]+=1;
             kindPiecesOriginal[4]+=1;
             break;
-    case 50 :
+        case 50 :
             kindPieces[5]+=1;
             kindPiecesOriginal[5]+=1;
             break;
-    case 100 :
+        case 100 :
             kindPieces[6]+=1;
             kindPiecesOriginal[6]+=1;
             break;
-    case 200 :
+        case 200 :
             kindPieces[7]+=1;
             kindPiecesOriginal[7]+=1;
             break;
             }
 
 }
-
     // on recherche les pieces a ramasser
     do{
         if(som >= 200 && kindPieces[7] > 0){
             som = som - 200;
             kindPieces[7] -= 1;
-
         }
         else if(som >= 100 && kindPieces[6] > 0){
             som = som - 100;
@@ -112,6 +110,7 @@ void RamaDoable(Ramapiece &p, int som, vector<int> &kindPiecesOriginal) {
         else
             break;
     }while(som>0 && !isEmpty(kindPieces));
+
     // calcul de la différence entre les deux tableaux
     for(int i = 0;i < 8; i++){
         kindPiecesOriginal[i]-=kindPieces[i];
