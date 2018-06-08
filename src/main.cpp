e39304c69ab053029d2f2bbad94a1750602fd45f
Okami <pierrick.garcia@hotmail.fr>
2018-06-08 14:17:11 +0200

    Add Value algorithms in release

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,16 @@
 #include "Ramapiece.hh"
 
 //// TMP
-std::vector<Piece>  *partie1(const std::string &filename) {
+std::vector<Piece>  *partie1(const std::string &filename)
+{
     std::vector<Piece>  *pieces = new std::vector<Piece>;
     for(int i = 0 ; i < 50 ; i++)
-        pieces->push_back(Piece(rand() % 200 + 1, rand() % 900 + 100, rand() % 900 + 100));
+        pieces->push_back(Piece(rand() % 200 + 1, rand() % (Ramapiece::IMAGE_WIDTH - 200) + 100, rand() % (Ramapiece::IMAGE_HEIGHT - 200) + 100));
     return (pieces);
 }
 
-static inline int   testAlgorithm(const Ramapiece &sample, int i) {
+static inline int   testAlgorithm(const Ramapiece &sample, int i)
+{
     int ret;
 
     Ramapiece   *ramapiece = new Ramapiece(sample);
@@ -42,6 +44,50 @@ void    pickUpPieces(Ramapiece &sample, const std::string &filename)
     sample.saveImage(filename);
 }
 
+static inline int   testAlgorithm(const Ramapiece &sample, int i, int toPick)
+{
+    int ret;
+
+    Ramapiece   *ramapiece = new Ramapiece(sample);
+    RamapieceAlgorithm::algosMoney[i].second(*ramapiece, toPick);
+    ret = ramapiece->getTraveledDistance();
+    delete ramapiece;
+    return ret;
+}
+
+void    pickUpPieces(Ramapiece &sample, const std::string &filename, int toPick)
+{
+    int i;
+    int minValue;
+    int value;
+    int idxMin = 0;
+
+    minValue = testAlgorithm(sample, 0, toPick);
+    i = 1;
+    while (RamapieceAlgorithm::algosMoney[i].first != "") {
+        value = testAlgorithm(sample, i, toPick);
+        if (value < minValue) {
+            minValue = value;
+            idxMin = i;
+        }
+        i += 1;
+    }
+    sample.initImage();
+    RamapieceAlgorithm::algosMoney[idxMin].second(sample, toPick);
+    sample.saveImage(filename);
+}
+
+/*std::vector<Piece>  *detection(const std::string &filename) {
+{
+    std::vector<Piece>  *pieces;
+
+    Detection detect;
+    detect.detectionPieces(filename);
+    pieces = new std::vector<Piece>(detect.getListePieceCourante());
+    quelCouleur(filename, *pieces);
+    return pieces;
+}*/
+
 int main(int ac, char **av)
 {
     if (ac == 3 || ac == 4) {
@@ -50,7 +96,7 @@ int main(int ac, char **av)
         if (ac == 3)
             pickUpPieces(ramapiece, av[2]);
         else if (ac == 4)
-            ;//pickUpValue(ramapiece, av[2], std::stoi(av[3]));
+            pickUpPieces(ramapiece, av[2], std::stoi(av[3]));
     }
     else {
         std::cerr << "Nombre d'arguments incorrects" << std::endl;
